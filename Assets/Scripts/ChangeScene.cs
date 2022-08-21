7d981729f18c4829799ab647ba1ffb23a914c161
Paula2208 <pguzmanz@unal.edu.co>
2022-08-21 01:03:11 -0500

    Changes on Change Scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public bool changeScene;
+    public int idScene;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Enter)){
+            SceneManager.LoadScene(idScene);
+        }
+    }
+
+    public void ChangeSceneEnter(int idScene){
+            SceneManager.LoadScene(idScene);
+    }
+}
